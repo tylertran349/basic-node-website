9e075329bf9bf351ac4088c0c8da9bd3fbe10927
tylertran349 <tylertran349@gmail.com>
2023-02-09 21:56:56 -0800

    Add URL redirection for home page

--- index.js
+++ index.js
@@ -9,24 +9,28 @@ http.createServer((req, res)=>{
             res.write(data);
             return res.end();
         });
-    }
-    else if(req.url === '/about'){
+    } else if(req.url === '/homepage'){
+        fs.readFile('index.html', function(err, data){
+            if (err) throw err;
+            res.writeHead(200,{'Content-Type': 'text/html'});
+            res.write(data);
+            return res.end();
+        });
+    } else if(req.url === '/about'){
         fs.readFile('about.html', function(err, data){
             if (err) throw err;
             res.writeHead(200,{'Content-Type': 'text/html'});
             res.write(data);
             return res.end();
         });
-    }
-    else if(req.url === '/contact-me'){
+    } else if(req.url === '/contact-me'){
         fs.readFile('contact-me.html', function(err, data){
             if(err) throw err;
             res.writeHead(200,{'Content-Type': 'text/html'});
             res.write(data);
             return res.end();
         });
-    }
-    else{
+    } else {
         fs.readFile('404.html', function(err, data){
             if(err) throw err;
             res.writeHead(200,{'Content-Type': 'text/html'});
