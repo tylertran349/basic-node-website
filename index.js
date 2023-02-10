e0c41492e3d3fafc78f4c43255dd2c03800db14a
tylertran349 <tylertran349@gmail.com>
2023-02-09 22:22:03 -0800

    Fix URL redirects

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 var http=require('http');
 var fs=require('fs');
 
-http.createServer((req, res)=>{
+http.createServer((req, res) => {
     if(req.url === '/'){
-        fs.readFile('index.html', function(err, data){
+        fs.readFile('index.html', function(err, data) {
             if(err) throw err;
             res.writeHead(200,{'Content-Type': 'text/html'});
             res.write(data);
-            return res.end();
+            return res.end;
         });
     } else if(req.url === '/about'){
-        fs.readFile('about.html', function(err, data){
+        fs.readFile('about.html', function(err, data) {
             if (err) throw err;
             res.writeHead(200,{'Content-Type': 'text/html'});
             res.write(data);
-            return res.end();
+            return res.end;
         });
     } else if(req.url === '/contact-me'){
-        fs.readFile('contact-me.html', function(err, data){
+        fs.readFile('contact-me.html', function(err, data) {
             if(err) throw err;
             res.writeHead(200,{'Content-Type': 'text/html'});
             res.write(data);
-            return res.end();
+            return res.end;
         });
     } else {
-        fs.readFile('404.html', function(err, data){
+        fs.readFile('404.html', function(err, data) {
             if(err) throw err;
             res.writeHead(200,{'Content-Type': 'text/html'});
             res.write(data);
-            return res.end();
+            return res.end;
         });
     }
-}).listen(8080, ()=>{
+}).listen(8080, () => {
     console.log('Listening on port 8080');
 });
